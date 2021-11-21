5f8d79b7d77318f6a91ecee8f15661b7892ff05a
Ethan C <ethan.cho1826@gmail.com>
2021-11-21 16:36:15 -0600

    Dog and User models

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: 'Email address is required',
+        unique: true,
+        trim: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please enter a valid email address"]
+    },
+    password: {
+        type: String,
+        require: "Password is required",
+        trim: true,
+        minLength: 7,
+        match: [/^[a-z0-9A-Z]+$/]
+    },
+    pets: {
+        type: Array
+    }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
