9ad8135db12b04b0577cbfbed47077b6e12b8792
Ethan C <ethan.cho1826@gmail.com>
2021-11-23 11:42:51 -0600

    seperated into client and server

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: 'Email address is required',
+        unique: true,
+        trim: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Please enter a valid email address"]
+    },
+    password: {
+        type: String,
+        require: "Password is required",
+        trim: true,
+        minLength: 7,
+        match: [/^[a-z0-9A-Z]+$/, "Password can only contain alphanumeric characters"]
+    },
+    pets: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Dog"
+        }
+    ]
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
