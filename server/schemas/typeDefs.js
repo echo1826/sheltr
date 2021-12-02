be73abf28cda0599f07dfd3857d4fb3515307be5
Mat Lundin <matmll12@gmail.com>
2021-12-02 13:18:00 -0600

    completed typeDefs for now

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,7 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     type User {
         _id: ID
+        username
         email: String
         pasword: String
         pets: [Dog] 
@@ -11,12 +12,38 @@ const typeDefs = gql`
     type Dog {
         _id: ID
         name: String
+        breed: {
+            primary: String
+            secondary: String
+            mixed: Boolean
+            unknown: Boolean
+        }
+        age: String
+        size: String
+        photo: {
+            small: String
+            medium: String
+            large: String
+            full: String
+        }
+        url: String
+        location: String
+        description: String
+        spayed: Boolean
+        house_trained: Boolean
+        shots: Boolean
+        organization: String
     }
     
     type Query {
         users: [User]
         dogs: [Dog]
         user(id: ID!): User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): User
+        updateUserPets(_id: ID!, pet: [Dog]): User
     }
     `;
 
