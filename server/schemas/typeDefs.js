762695cd8970dd683fa048225a19b5fe0bc069c3
Ethan C <ethan.cho1826@gmail.com>
2021-12-01 23:48:22 -0600

    updated typedefs to match the dog model

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,7 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     type User {
         _id: ID
+        name: String
         email: String
         pasword: String
         pets: [Dog] 
@@ -11,6 +12,22 @@ const typeDefs = gql`
     type Dog {
         _id: ID
         name: String
+        breed: {
+            primary: String
+            secondary: String
+            mixed: Boolean
+            unknown: Boolean
+        }
+        age: String
+        size: String
+        photo: [{small: String, medium: String, large: String, full: String}]
+        url: String
+        location: String
+        description: String
+        spayed: Boolean
+        house_trained: Boolean
+        shots: Boolean
+        organization: String
     }
     
     type Query {
