3d3d74adc26302eea883d0b9bc3f4f8841554f82
Sue Lee <sue.lee0308@gmail.com>
2021-12-02 14:15:15 -0600

    navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,10 +1,41 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import Auth from "../../utils/auth";
+import { Link } from "react-router-dom";
+
 
 export default function Navbar() {
-
-
-    return(<div>
-        
-    </div>);
+    if (Auth.loggedIn()) {
+    return(
+    <nav className='navContainer'>
+        <ul>
+            <li>
+                <Link to='/profile'>
+                    <img src='./images/profileblank.png' alt='profile'></img>
+                    Profile
+                </Link>
+            </li>
+            <li>
+                <Link to='/sheltr'>
+                    <img src='./images/pawblank.png' alt='sheltr'></img>
+                    Sheltr
+                </Link>
+            </li>
+            <li>
+                <Link to='/likes'>
+                    <img src='./images/heartblank.png' alt='heart'></img>
+                    Likes
+                </Link>
+            </li>
+            <li>
+                <Link to='/settings'>
+                    <img src='./images/settingsblank.png' alt='settings'></img>
+                    Settings
+                </Link>
+            </li>
+        </ul>
+    </nav>
+    )
+    } else {
+        return (<Login />)
+    }
 }
