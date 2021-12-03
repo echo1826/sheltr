6ed5e0a7741c67bebfe425f398150e987fcb23ab
Sue Lee <sue.lee0308@gmail.com>
2021-12-03 14:19:15 -0600

    likes, profile and cards pages/components. not tested

--- client/src/pages/Cards.js
+++ client/src/pages/Cards.js
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+// need to import all dogs returned after filtering from user settings
 
 export default function Cards() {
 
 
     return(<div>
         
+        {/* {userArrDogs.map((dog) => {
+            <div key={dog._id}>
+                <img src={dog.photo[0].small} alt="dog photo"></img>
+                <h2>{dog.name}</h2>
+                <p>{dog.breed.primary} | {dog.location}</p>
+                <ul>
+                    <li>size: {dog.size}</li>
+                    <li>gender: {dog.gender}</li>
+                    <li>age: {dog.age}</li>
+                </ul>
+            </div>
+        })}
+        <div>
+            <button id="nope"></button>
+            <button id="like"></button>
+        </div> */}
+
     </div>);
 }
--- client/src/pages/Likes.js
+++ client/src/pages/Likes.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import LikedCards from '../components/LikedCards/index';
+import { Grid } from '@mui/material';
 
 export default function Likes() {
 
-
-    return(<div>
-        
-    </div>);
+    return(
+ 
+    <Grid container spacing={2}
+        container direction="row"
+        justifyContent="center"
+        alignItems="center"
+    >
+   
+        <h1>Your Liked Dogs</h1>
+        <LikedCards />
+    
+    </Grid>
+  
+    )
 }
