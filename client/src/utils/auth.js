4061141ce2c4c971f7845f0bd1819d07ca655072
Phillip <phillip.welch2@mavs.uta.edu>
2021-12-03 10:32:24 -0600

    Created the client auth.js file

---
+++ client/src/utils/auth.js
@@ -0,0 +1,44 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+    //Saves user token
+    login() {
+        localStorage.setItem('id_token');
+        
+        window.location.assign('/');
+    }
+    //deletes user token
+    logout() {
+        localStorage.removeItem('id_token');
+        window.location.assign('/');
+    }
+    //get user token from localstorage
+    getToken() {
+        return localStorage.getItem('token_id');
+    }
+
+    checkTokenExpiration() {
+        try {
+            const decoded = decode(token);
+
+            if(decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch(err) {
+            return false;
+        }
+    }
+
+    getProfileToken() {
+        return decode(this.getProfileToken());
+    }
+
+    isLoggedIn() {
+        const token = this.getToken();
+        return !!token && !this.checkTokenExpiration(token);
+    }
+};
+
+export default new ();
