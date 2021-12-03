833ea5098d7b949060925e29f98e4087b638e97b
Mat Lundin <matmll12@gmail.com>
2021-12-03 13:08:48 -0600

    signup component initialized

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 export default function Signup() {
 
 
-    return(<div>
-
+    return (<div>
+        <FormControl>
+            <InputLabel htmlFor="my-input">Email</InputLabel>
+            <Input id="email" aria-describedby="my-helper-text" />
+            <FormHelperText id="email helper">We'll never share your email.</FormHelperText>
+            <InputLabel htmlFor="my-input">Username</InputLabel>
+            <Input id="username" aria-describedby="my-helper-text" />
+            <FormHelperText id="username helper">Username helper text</FormHelperText>
+            <InputLabel htmlFor="my-input">Password</InputLabel>
+            <Input id="password" aria-describedby="my-helper-text" />
+            <FormHelperText id="password helper">Passwords must be at least 7 characters, alphanumeric only.</FormHelperText>
+        </FormControl>
     </div>);
 }
