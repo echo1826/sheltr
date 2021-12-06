13468c1564941137054fbd228283bfc1bf153ed4
Ethan C <ethan.cho1826@gmail.com>
2021-12-06 16:09:35 -0600

    going through react warning and getting rid of them, settings object created when user is created

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -121,6 +121,7 @@ const resolvers = {
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
+            await Settings.create({userId: user._id, age: null, size: null, house_trained: null});
             console.log(user);
             const token = signToken(user);
             return { token, user };
