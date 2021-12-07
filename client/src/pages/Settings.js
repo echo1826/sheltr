5e6e4c851464733f54e0abdc85de18b6b3fdbfb0
Sue Lee <sue.lee0308@gmail.com>
2021-12-07 12:20:49 -0600

    SETTINGS STYLED JC

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -80,15 +80,17 @@ export default function Settings() {
         gridAutoColumns: '1fr',
         gap: 3,
         alignItems: 'center',
+        paddingBottom: 7,
       }}
     >
       <Paper className='settingsContainer' elevation = {6} >
           <h2 align="center">User Settings</h2>
-        <FormControl fullWidth>
-          <InputLabel id="age-select" className='settingsForm'>Age</InputLabel>
+  
+        <FormControl fullWidth style={{marginBottom: '20px'}} className='settingsForm'>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
           <Select
-            labelId="age-select"
-            id="demo-simple-select"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select-label"
             value={prevAge}
             label="Age"
             type="age"
@@ -102,8 +104,9 @@ export default function Settings() {
             <MenuItem value={'senior'}>Senior</MenuItem>
           </Select>
         </FormControl>
-        <FormControl fullWidth>
-          <InputLabel id="size-select" margin="10%">Size</InputLabel>
+    
+        <FormControl fullWidth fullWidth style={{marginTop: '20px', marginBottom:'10px'}} className='settingsForm'>
+          <InputLabel id="size-select">Size</InputLabel>
           <Select
             labelId="size-select"
             id="demo-simple-select"
