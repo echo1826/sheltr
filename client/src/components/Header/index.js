95fb49c87ae06a33628c44e0c56d8f875d709734
Chuck Stephens <chuckstephens2076@gmail.com>
2021-12-07 10:49:32 -0600

    adjusted cards and added new logo color

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -12,7 +12,7 @@ export default function ButtonAppBar() {
     <Box sx={{ }}>
       <AppBar sx = {{ position:"static", backgroundColor:"transparent"}} >
         <Toolbar className='sheltrHeader'>
-        <img src="./images/sheltr_logo.png" alt="Sheltr" className='headerImg'/>
+        <img src="./images/sheltr_logo2.png" alt="Sheltr" className='headerImg'/>
          <a href='/sheltr' className='headerText' style={{ textDecoration:'none', color:'#000' }}>Sheltr</a>
         </Toolbar>
       </AppBar>
