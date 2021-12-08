ab67f05715ff50382c6a0f0be5dbadc81ad76ed6
Ethan C <ethan.cho1826@gmail.com>
2021-12-08 14:31:52 -0600

    please deploy

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db = require('./config/connection');
 const {ApolloServer} = require('apollo-server-express');
 const {typeDefs, resolvers} = require('./schemas');
 const { authMiddleware } = require("./utils/auth");
-// const path = require('path');
+const path = require('path');
 // const routes = require('./controllers');
 
 
