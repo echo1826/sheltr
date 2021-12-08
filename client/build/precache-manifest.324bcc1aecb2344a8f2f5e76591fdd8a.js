251652dd7f26745e150fb84fb2e8d0fc7ac61298
Phillip <phillip.welch2@mavs.uta.edu>
2021-12-08 13:57:17 -0600

    pushing build

---
+++ client/build/precache-manifest.324bcc1aecb2344a8f2f5e76591fdd8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71fbc3a54c58e441beaa446c9a96175",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c07ad4ebe8f08c4c1449",
+    "url": "/static/css/main.f96af275.chunk.css"
+  },
+  {
+    "revision": "a6ba9ace666d484b5995",
+    "url": "/static/js/2.11ae1137.chunk.js"
+  },
+  {
+    "revision": "48a8bcc2ee56e71f1a24fb8975531992",
+    "url": "/static/js/2.11ae1137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e896b18767d680ad1f9f",
+    "url": "/static/js/3.5d98a725.chunk.js"
+  },
+  {
+    "revision": "c07ad4ebe8f08c4c1449",
+    "url": "/static/js/main.e005e83a.chunk.js"
+  },
+  {
+    "revision": "7134673f700f2f9c970c",
+    "url": "/static/js/runtime-main.74e047a1.js"
+  }
+]);
