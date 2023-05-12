39083f55c1ecf5273642300ffc200c79a4756409
Ethan <ethan.cho1826@gmail.com>
2023-05-11 19:14:29 -0500

    fixed folder paths for server to respond with build

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 // Start the server
